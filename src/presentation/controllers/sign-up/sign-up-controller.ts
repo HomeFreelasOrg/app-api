85a8fe9505dd422e7fc7761fb7a8c0cd5f88b429
IsraelCost <cmteisrael@gmail.com>
2022-06-04 23:14:02 -0300

    feat: ensure SignUpController returns a bad request error if missing params

--- src/presentation/controllers/sign-up/sign-up-controller.ts
+++ src/presentation/controllers/sign-up/sign-up-controller.ts
@@ -1,7 +1,30 @@
+import { MissingParamError } from "../../errors";
+import { badRequest } from "../../helpers";
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
 
 export class SignUpController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const requiredFields = [
+      "name",
+      "age",
+      "birth",
+      "address",
+      "email",
+      "password",
+    ];
+
+    const missingFields = [];
+
+    for (let requiredField of requiredFields) {
+      if (!httpRequest.body[requiredField]) {
+        missingFields.push(requiredField);
+      }
+    }
+
+    if (missingFields.length > 0) {
+      return badRequest(new MissingParamError(missingFields));
+    }
+
     return new Promise((res) => res(null));
   }
 }
