6c7b20b36cb6d9c41cd0dcceeeff087ac5e0a7c9
IsraelCost <cmteisrael@gmail.com>
2022-06-04 23:13:16 -0300

    feat: add missing param error

---
+++ src/presentation/errors/missing-param-error.ts
@@ -0,0 +1,11 @@
+export class MissingParamError extends Error {
+  constructor(paramsNames: string[]) {
+    const [firstParam, ...restOfParams] = paramsNames;
+    super(
+      `Missing the following params: ${restOfParams.map(
+        (param) => `${param}, `
+      )}${firstParam}.`
+    );
+    this.name = "MissingParamError";
+  }
+}
