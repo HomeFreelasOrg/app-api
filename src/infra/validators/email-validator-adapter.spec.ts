500e18c0dbdcddf9a2d1c112cfc51d49971d096c
IsraelCost <cmteisrael@gmail.com>
2022-06-06 17:04:18 -0300

    test: ensure EmailValidatorAdapters calls validator with correct values and return the returned value by validator

--- src/infra/validators/email-validator-adapter.spec.ts
+++ src/infra/validators/email-validator-adapter.spec.ts
@@ -1,9 +1,38 @@
 import { EmailValidatorAdapter } from '.'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isEmail (): boolean {
+    return true
+  }
+}))
 
 describe('EmailValidatorAdapter', () => {
   test('Should call with correct value', () => {
     const sut = new EmailValidatorAdapter()
     const validateSpy = jest.spyOn(sut, 'validate')
+    sut.validate('any_mail')
+    expect(validateSpy).toHaveBeenCalledWith('any_mail')
+  })
+
+  test('Should return false if validator returns false', () => {
+    const sut = new EmailValidatorAdapter()
+    const validateSpy = jest.spyOn(validator, 'isEmail')
+    validateSpy.mockReturnValueOnce(false)
+    const isValidEmail = sut.validate('any_mail')
+    expect(isValidEmail).toBe(false)
+  })
+
+  test('Should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    jest.spyOn(validator, 'isEmail')
+    const isValidEmail = sut.validate('any_mail')
+    expect(isValidEmail).toBe(true)
+  })
+
+  test('Should call validator with correct value', () => {
+    const sut = new EmailValidatorAdapter()
+    const validateSpy = jest.spyOn(validator, 'isEmail')
     sut.validate('any_mail')
     expect(validateSpy).toHaveBeenCalledWith('any_mail')
   })
