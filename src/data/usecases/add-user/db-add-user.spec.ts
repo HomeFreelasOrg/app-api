8868d9d5b04d820cec8353b9d8415b13216a32f3
IsraelCost <cmteisrael@gmail.com>
2022-06-07 20:18:04 -0300

    test: ensure DBAddUser throws a UnderageError if user age is less than 16

--- src/data/usecases/add-user/db-add-user.spec.ts
+++ src/data/usecases/add-user/db-add-user.spec.ts
@@ -1,4 +1,5 @@
 import { AddUser } from '../../../domain/usecases'
+import { UnderageError } from '../../errors'
 import { DBAddUser } from './db-add-user'
 
 interface SutTypes {
@@ -10,7 +11,7 @@ const fakerUser: AddUser.Params = {
   age: 'any_age',
   address: 'any_address',
   email: 'any_mail@mail.com',
-  birth: new Date(2022, 9, 5),
+  birth: new Date(2000, 9, 5),
   password: 'any_password'
 }
 
@@ -22,10 +23,19 @@ const makeSut = (): SutTypes => {
 }
 
 describe('DBAddUser', () => {
-  test('Should call with correct values', () => {
+  test('Should call with correct values', async () => {
     const { sut } = makeSut()
     const addSpy = jest.spyOn(sut, 'add')
-    sut.add(fakerUser)
+    await sut.add(fakerUser)
     expect(addSpy).toHaveBeenCalledWith(fakerUser)
+  })
+
+  test('Should return an UnderAgeError if the user\'s age is less than 16 years', async () => {
+    try {
+      const { sut } = makeSut()
+      await sut.add({ ...fakerUser, birth: new Date(2008, 8, 12) })
+    } catch (error) {
+      expect(error).toEqual(new UnderageError())
+    }
   })
 })
