54f98f71f57b7f95dc01384916d63669abcf57fb
IsraelCost <cmteisrael@gmail.com>
2022-06-08 11:40:44 -0300

    feat: ensure DBAddUser calls encrypter with correct password

--- src/data/usecases/add-user/db-add-user.ts
+++ src/data/usecases/add-user/db-add-user.ts
@@ -1,14 +1,19 @@
 import { AddUser } from '../../../domain/usecases'
 import { UnderageError } from '../../errors'
+import { Encrypter } from '../../protocols'
 import { getAgeByBirth } from '../../services'
 
 export class DBAddUser implements AddUser {
+  constructor (private readonly encrypter: Encrypter) {}
+
   async add (user: AddUser.Params): Promise<AddUser.Result> {
     const age = getAgeByBirth(user.birth)
 
     if (age < 16) {
       return new Promise((resolve, reject) => reject(new UnderageError()))
     }
+
+    this.encrypter.encrypt(user.password)
 
     return new Promise((resolve) => resolve(null))
   }
